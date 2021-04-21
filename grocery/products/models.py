5b5b0d282b756013675bc33472744d16a70b99e6
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-21 12:47:12 +0530

    Added Product, Category, Offer Modules

--- grocery/products/models.py
+++ grocery/products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ManyToManyField('Category')
+    isOffer = models.BooleanField(default=False)
+    offer = models.ForeignKey(
+        'Offer', on_delete=models.CASCADE, blank=True, null=True)
+    stockCount = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Offer(models.Model):
+    description = models.CharField(max_length=100)
+    percentage = models.IntegerField()
+    start = models.DateTimeField(auto_now_add=True)
+    expiry = models.DateTimeField()
+
+    def __str__(self):
+        return self.description
