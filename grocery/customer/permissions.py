b3b16216d9cdd388a6c1258e3ff84f8c3bc5a057
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-22 21:36:03 +0530

    Added Custom Permission for Views

--- grocery/customer/permissions.py
+++ grocery/customer/permissions.py
@@ -8,3 +8,12 @@ class IsOwnerProfile(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsOwnerCustomer(BasePermission):
+    """
+    Object-level permission to only allow owners of an object to access it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.profile.user == request.user
