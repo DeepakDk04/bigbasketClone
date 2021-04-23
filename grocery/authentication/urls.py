d694d6e0951d43fdd26e9035f8951867f44e0855
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-23 16:28:46 +0530

    added register login logout api

--- grocery/authentication/urls.py
+++ grocery/authentication/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from . import views
+from .views import RegisterAPIView, LoginAPIView
+from knox.views import LogoutView, LogoutAllView
+
 
 urlpatterns = [
-    path('',),
+
+    path('register/', RegisterAPIView.as_view()),
+    path('login/', LoginAPIView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('logoutall/', LogoutAllView.as_view()),
+
 ]
--- grocery/authentication/views.py
+++ grocery/authentication/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from django.contrib.auth import login
+
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
+
+from .serializers import UserSerializer, RegisterSerializer
 
 # Create your views here.
+
+# Register API
+
+
+class RegisterAPIView(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class LoginAPIView(KnoxLoginView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        login(request, user)
+
+        return super(LoginAPIView, self).post(request, format=None)
