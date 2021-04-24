52aa51bae392c21f3bf32718b1975ed4f785c4e5
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-24 20:27:00 +0530

    registerd models in admin site

--- grocery/deliveryservice/admin.py
+++ grocery/deliveryservice/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import DeliveryServicer, DeliveryServicerProfile
 # Register your models here.
+
+admin.site.register(DeliveryServicer)
+admin.site.register(DeliveryServicerProfile)
