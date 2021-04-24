fb16a92b84768e79f52c241178db2e80b72e51c8
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-24 20:27:41 +0530

    added str representaion to models

--- grocery/deliveryservice/models.py
+++ grocery/deliveryservice/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class DeliveryServicer(models.Model):
+    rating = [("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5")]
+    profile = models.OneToOneField(
+        'DeliveryServicerProfile', on_delete=models.CASCADE, default='')
+    ratings = models.CharField(max_length=1, choices=rating)
+
+    def __str__(self):
+        return self.profile.user.username
+
+
+class DeliveryServicerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    age = models.IntegerField()
+    contactno = models.CharField(max_length=10)
+    # ordersTaken = models.ManyToManyField("Order", null=True)
+
+    def __str__(self):
+        return self.user.username
