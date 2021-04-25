bbbf3f7ab6c89ea51957407f3979439999a3e610
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-25 19:28:25 +0530

    added direct imports

--- grocery/products/urls.py
+++ grocery/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('all/', views.ProductListView.as_view() ),
-    path('detail/<int:id>/', views.ProductDetailView.as_view() ),
+    path('all/', ProductListView.as_view()),
+    path('detail/<int:id>/', ProductDetailView.as_view()),
 ]
