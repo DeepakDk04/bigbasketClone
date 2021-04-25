e6fc818d4d1eaef79f874d45b80a42d18c4de457
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-25 19:31:33 +0530

    included order.urls in main urls.py

--- grocery/grocery/urls.py
+++ grocery/grocery/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,5 +24,6 @@ urlpatterns = [
     path('customer/', include('customer.urls')),
     path('auth/', include('authentication.urls')),
     path('delivery/', include('deliveryservice.urls')),
+    path('order/', include('order.urls')),
 
 ]
