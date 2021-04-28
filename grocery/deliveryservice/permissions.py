3e21b255357df584052d61dcc9ec73d4c6c4c02f
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-28 17:55:53 +0530

    added 2 more custom permissions

--- grocery/deliveryservice/permissions.py
+++ grocery/deliveryservice/permissions.py
@@ -17,3 +17,30 @@ class IsOwnerDeliverProfile(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsDeliverGroup(BasePermission):
+    """
+    Object-level permission to only allow owners of an object to access it.
+    Only A user in a deliverservicer group can create the deliverservicer account.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.groups.filter(name="Deliveryservicer").exists()
+
+
+class IsOrderedCustomer(BasePermission):
+    """
+    Object-level permission to only allow owners of an object to access it.
+    Only the customer who placed order and recieved from the delivery guy can tell ratings of delivery servicer.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        deliveries = list(obj.mydeliveries.all())
+
+        for order in deliveries:
+            if order.orderbycustomer.profile.user == request.user:
+                return True
+
+        return False
