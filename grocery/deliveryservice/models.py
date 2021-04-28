5f9275aff7d6abc4851eab6c94456526ea9c2608
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-28 17:54:48 +0530

    added mydeliveries,availabe, gender on meodel

--- grocery/deliveryservice/models.py
+++ grocery/deliveryservice/models.py
@@ -6,22 +6,37 @@ from django.db import models
 
 
 class DeliveryServicer(models.Model):
+    '''
+    Delivery Servicer Account
+    '''
     rating = [(0, 0), (1, 1), (2, 2),
               (3, 3), (4, 4), (5, 5)]
+
     profile = models.OneToOneField(
         'DeliveryServicerProfile', on_delete=models.CASCADE, default='')
     ratings = models.IntegerField(
         choices=rating, default=0, blank=True, null=True)
+    mydeliveries = models.ManyToManyField(
+        "order.Order", blank=True, default='0')
+
+    available = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         return self.profile.user.username
 
 
 class DeliveryServicerProfile(models.Model):
+    '''
+    Delivery Servicer Profile
+    '''
+    genderchoices = [
+        ("Male", "Male"), ("Female", "Female"), ("Others", "Others")
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.IntegerField()
+    gender = models.CharField(max_length=6, choices=genderchoices)
     contactno = models.CharField(max_length=10)
-    # ordersTaken = models.ManyToManyField("Order", null=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.username + ' , age ' + str(self.age)
