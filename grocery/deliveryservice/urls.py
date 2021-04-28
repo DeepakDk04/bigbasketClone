10fc10f3c648977c646ee4585095be8950058ce9
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-28 17:59:01 +0530

    removed specific profile detail url

--- grocery/deliveryservice/urls.py
+++ grocery/deliveryservice/urls.py
@@ -7,12 +7,14 @@ from .views import (
     DeliverServicerSignUpAPIView,
 
     DeliverServicerProfileCreateView,
-    DeliverServicerProfileDetailView,
+    # DeliverServicerProfileDetailView,
     DeliverServicerProfileUpdateView,
 
     DeliverServicerCreateView,
     DeliverServicerDetailView,
-    DeliverServicerUpdateView,
+    DeliverServicerRatingsUpdateView,
+    DeliverServicerDeliveryUpdateView,
+    DeliverServicerAvailableUpdateView,
 
 )
 
@@ -22,14 +24,20 @@ urlpatterns = [
     path('servicer-profile/create/', DeliverServicerProfileCreateView.as_view()),
     path('servicer/create/', DeliverServicerCreateView.as_view()),
 
-    path('servicer-profile/detail/<int:id>/',
-         DeliverServicerProfileDetailView.as_view()),
+    # path('servicer-profile/detail/<int:id>/',
+    #      DeliverServicerProfileDetailView.as_view()),
     path('servicer/detail/<int:id>/', DeliverServicerDetailView.as_view()),
 
     path('user-update/<int:id>/', UserUpdateView.as_view()),
     path('servicer-profile/update/<int:id>/',
          DeliverServicerProfileUpdateView.as_view()),
-    path('servicer/update/<int:id>/', DeliverServicerUpdateView.as_view()),
+
+    path('servicer/update-ratings/<int:id>/',
+         DeliverServicerRatingsUpdateView.as_view()),
+    path('servicer/update-delivery/<int:id>/',
+         DeliverServicerDeliveryUpdateView.as_view()),
+    path('servicer/update-available/<int:id>/',
+         DeliverServicerAvailableUpdateView.as_view()),
 
 
 ]
