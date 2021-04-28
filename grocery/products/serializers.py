4b4d8f1fd8c9925d388612532e831dff281ef364
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-28 18:02:21 +0530

    added price field on serializer

--- grocery/products/serializers.py
+++ grocery/products/serializers.py
@@ -5,14 +5,12 @@ from rest_framework.serializers import ModelSerializer
 class ProductListSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'offer', 'stockCount']
-
+        fields = ('id', 'name', 'category', 'offer', 'stockCount', 'price')
 
 
 class ProductDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'offer', 'stockCount']
+        fields = ('id', 'name', 'category', 'offer', 'stockCount', 'price')
         depth = 1
-        
