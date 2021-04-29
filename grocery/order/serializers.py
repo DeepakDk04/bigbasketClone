8c16095688eebfcd0495b7f1ce5d1364984b18bd
DeepakDK04 <deepak99pmk@gmail.com>
2021-04-29 23:16:01 +0530

    added multilevel nested serializers-order response

--- grocery/order/serializers.py
+++ grocery/order/serializers.py
@@ -1,16 +1,67 @@
+from django.contrib.auth.models import User
 from django.db import models
 from rest_framework import fields
 from rest_framework.serializers import ModelSerializer
 
-from .models import Cart, CartItem, Order
+from .models import Cart, CartItem, Order, OrderItem
+from products.models import Product
+from deliveryservice.models import DeliveryServicer, DeliveryServicerProfile
+from customer.models import DeliveryAddress
+
+
+class UserDisplaySerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name')
+
+
+class OrderResponseDisplay__OrderShipper__ProfileSerializer(ModelSerializer):
+    user = UserDisplaySerializer()
+
+    class Meta:
+        model = DeliveryServicerProfile
+        exclude = ('id',)
+
+
+class OrderResponseDisplay__OrderShipperSerializer(ModelSerializer):
+
+    profile = OrderResponseDisplay__OrderShipper__ProfileSerializer()
+
+    class Meta:
+        model = DeliveryServicer
+        fields = ('ratings', 'profile')
+        depth = 1
+
+
+class OrderResponseDisplay__ToAddressSerializer(ModelSerializer):
+
+    class Meta:
+        model = DeliveryAddress
+        exclude = ('id',)
+
+
+class OrderResponseDisplaySerializer(ModelSerializer):
+
+    ordershipper = OrderResponseDisplay__OrderShipperSerializer()
+    toaddress = OrderResponseDisplay__ToAddressSerializer()
+
+    class Meta:
+        model = Order
+        # fields = ('id', 'items', 'orderbycustomer',
+        #           'ordershipper', 'amount', 'toaddress', 'status')
+        exclude = ('id', 'items', 'orderbycustomer')
+        depth = 1
 
 
 class OrderCreateSerializer(ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ('id', 'items', 'orderbycustomer',
-                  'ordershipper', 'amount', 'toaddress', 'status')
+        fields = '__all__'
+        # fields = ('id', 'items', 'orderbycustomer',
+        #           'ordershipper', 'amount', 'toaddress', 'status')
+
 
 # {
 # 	"items":[
@@ -27,13 +78,30 @@ class CartItemSerializer(ModelSerializer):
         fields = ("id", "product", "quantity")
 
 
+###
+class Cart__Items__ProductSerializer(ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('name',)
+
+
+class Cart__ItemsSerializer(ModelSerializer):
+
+    product = Cart__Items__ProductSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = "__all__"
+
+
 class CartUpdateSerializer(ModelSerializer):
 
-    # items = CartItemSerializer()
+    items = Cart__ItemsSerializer(many=True)
 
     class Meta:
         model = Cart
-        fields = ["id", "items"]
+        fields = "__all__"
         depth = 1
 
 # {
@@ -48,6 +116,13 @@ class CartUpdateSerializer(ModelSerializer):
 #         },
 #     ]
 # }
+
+
+class OrderItemCreateSerializer(ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
 
 
 class OrderStatusUpdateSerializer(ModelSerializer):
