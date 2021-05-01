9ad7e0b968b8bb2b329a70ce09f5860f332a1386
DeepakDK04 <deepak99pmk@gmail.com>
2021-05-01 22:50:45 +0530

    added signup view template

--- grocery/shopowner/views.py
+++ grocery/shopowner/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from shopowner.models import ShopOwner
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 # Create your views here.
+
+
+class OwnerAccountSignUpAPIView(CreateAPIView):
+
+    queryset = ShopOwner.objects.all()
+    permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
