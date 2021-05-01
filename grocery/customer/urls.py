5f88abd00eac2011553eca97e6b35c14db22a570
DeepakDK04 <deepak99pmk@gmail.com>
2021-05-01 20:52:59 +0530

    cmntd unused, added check-ordr-delvry url

--- grocery/customer/urls.py
+++ grocery/customer/urls.py
@@ -6,16 +6,18 @@ from .views import (
     CustomerDetailView,
     CustomerUpdateView,
 
+    CustomerPlacedOrdersProgressView,
+
     # CustomerProfileCreateView,
-    CustomerProfileDetailView,
-    UserUpdateView,
-    CustomerProfileUpdateView,
-    CustomerProfileDeleteView,
+    # CustomerProfileDetailView,
+    # UserUpdateView,
+    # CustomerProfileUpdateView,
+    # CustomerProfileDeleteView,
 
     # AddressCreateView,
-    AddressDetailView,
-    AddressUpdateView,
-    AddressDeleteView,
+    # AddressDetailView,
+    # AddressUpdateView,
+    # AddressDeleteView,
 
 )
 
@@ -23,21 +25,23 @@ from .views import (
 urlpatterns = [
 
     # path('profil-create/', CustomerProfileCreateView.as_view()),
-    path('profile-detail/<int:id>/', CustomerProfileDetailView.as_view()),
+    # path('profile-detail/<int:id>/', CustomerProfileDetailView.as_view()),
 
-    path('user-update/<int:id>/', UserUpdateView.as_view()),
-    path('profile-update/<int:id>/', CustomerProfileUpdateView.as_view()),
+    # path('user-update/<int:id>/', UserUpdateView.as_view()),
+    # path('profile-update/<int:id>/', CustomerProfileUpdateView.as_view()),
     # when calling profile-update, profile-user-update must called before if user property changed
 
     # path('profile-delete/<int:id>/', CustomerProfileDeleteView.as_view()),
 
     # path('address-create/', AddressCreateView.as_view()),
-    path('address-detail/<int:id>/', AddressDetailView.as_view()),
-    path('address-update/<int:id>/', AddressUpdateView.as_view()),
-    path('address-delete/<int:id>/', AddressDeleteView.as_view()),
+    # path('address-detail/<int:id>/', AddressDetailView.as_view()),
+    # path('address-update/<int:id>/', AddressUpdateView.as_view()),
+    # path('address-delete/<int:id>/', AddressDeleteView.as_view()),
 
     path('create/', CustomerCreateView.as_view()),
     path('detail/<int:id>/', CustomerDetailView.as_view()),
     path('update/<int:id>/', CustomerUpdateView.as_view()),
+    path('check-order-delivery/<int:id>/',
+         CustomerPlacedOrdersProgressView.as_view()),
 
 ]
