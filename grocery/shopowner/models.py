d18f58a7597496f3a87970d81ad30c447d5503d1
DeepakDK04 <deepak99pmk@gmail.com>
2021-05-01 22:14:44 +0530

    added models and registered with admin

--- grocery/shopowner/models.py
+++ grocery/shopowner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class ShopOwner(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+
+
+class JoinCode(models.Model):
+    code = models.CharField(max_length=10, unique=True)
