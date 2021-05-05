046f7edea300d54261b6edeebc7201052a03477a
DeepakDK04 <deepak99pmk@gmail.com>
2021-05-05 13:53:41 +0530

    added smtp configuration

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -166,3 +166,12 @@ REST_KNOX = {
     # 'AUTO_REFRESH': False,
     # 'EXPIRY_DATETIME_FORMAT': api_settings.DATETME_FORMAT,
 }
+
+
+# SMPT configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_ID')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
